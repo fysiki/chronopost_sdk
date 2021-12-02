0a6b00154100338d71a30c79ce2a6e4c2ac6f512
ebelk <killian.ebel@fysiki.com>
2021-12-02 09:07:33 +0100

    Add tracking WS

---
+++ generate-tracking.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+/** @var WsdlToPhp\PackageGenerator\ConfigurationReader\GeneratorOptions */
+$options = (WsdlToPhp\PackageGenerator\ConfigurationReader\GeneratorOptions::instance());
+$options
+    ->setOrigin('https://ws.chronopost.fr/tracking-cxf/TrackingServiceWS?wsdl')
+    ->setDestination('.')
+    ->setStandalone(false)
+    ->setComposerName('fysiki/chronopost_sdk')
+    ->setNamespace('Chronopost')
+;
+// Generator instantiation
+$generator = new WsdlToPhp\PackageGenerator\Generator\Generator($options);
+// Package generation
+$generator->generatePackage();
